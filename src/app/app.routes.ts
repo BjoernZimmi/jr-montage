499b37ca246cab1988a9b4720a96baefce201f36
Björn Zimmer <bjoernz@live.de>
2025-05-28 20:23:58 +0200

    added alle files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ImpressumComponent } from './component/impressum/impressum.component';
+import { StartpageComponent } from './component/startpage/startpage.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StartpageComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
