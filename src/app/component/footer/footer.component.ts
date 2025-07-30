fffc4c9584da1a6265588671c9c6ccc114096873
Björn Zimmer <bjoernz@live.de>
2025-07-30 19:30:18 +0200

    added footer header and logo

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatGridListModule, MatDividerModule, RouterModule],
   templateUrl: './footer.component.html',
   styles: ``
 })
+
 export class FooterComponent {
+  currentYear = new Date().getFullYear();
 
 }
--- src/app/component/startpage/startpage.component.ts
+++ src/app/component/startpage/startpage.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-startpage',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './startpage.component.html',
   styles: ``
 })
