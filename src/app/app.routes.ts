60ee06fdf6380df727c2746cb36da891f944990e
Björn Zimmer <bjoernz@live.de>
2025-09-02 00:17:25 +0200

    change styling and icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ImpressumComponent } from './component/impressum/impressum.component';
 import { StartpageComponent } from './component/startpage/startpage.component';
 import { LeistungenComponent } from './component/leistungen/leistungen.component';
+import { KontaktComponent } from './component/kontakt/kontakt.component';
 
 export const routes: Routes = [
   { path: '', component: StartpageComponent },
   { path: 'impressum', component: ImpressumComponent },
   { path: 'leistungen', component: LeistungenComponent },
+  { path: 'kontakt', component: KontaktComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
