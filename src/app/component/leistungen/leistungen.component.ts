ca4bfc098e27b3baec027e91aac383ff82da4d4d
Björn Zimmer <bjoernz@live.de>
2025-09-04 02:27:59 +0200

    added startpage, leistungen, datenshutz

--- src/app/component/leistungen/leistungen.component.ts
+++ src/app/component/leistungen/leistungen.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-leistungen',
-  imports: [],
+  imports: [MatExpansionModule, RouterLink],
   templateUrl: './leistungen.component.html'
 })
 export class LeistungenComponent {
 
+  services = [
+    {
+      id: 'fussboden',
+      title: 'Fußbodenverlegung',
+      intro: 'Verlegen von Laminat, Vinyl und Parkett – sauber, präzise und zuverlässig.',
+      image: 'assets/img/fussboden.jpg',
+      points: [
+        'Untergrundprüfung & Vorbereitung',
+        'Schwimmend oder verklebt, sauber zugeschnitten',
+        'Leisten & Übergangsprofile, Randlösungen'
+      ],
+      gallery: [
+        { src: 'assets/img/fussboden_detail.jpg', alt: 'Kleberbett – Detail' },
+        { src: 'assets/img/fussboden_raum.jpg',   alt: 'Fertig verlegter Eichenboden' }
+      ]
+    },
+    {
+      id: 'terrasse',
+      title: 'Terrassenbau & Gartenhaus',
+      intro: 'Holzterrassen inkl. Unterkonstruktion – robust, eben und wetterfest.',
+      image: 'assets/img/terrasse.jpg',
+      points: [
+        'Stabile Unterkonstruktion mit Gefälle',
+        'Riffel- oder Glattholzdielen, verdeckte Verschraubung möglich',
+        'Pflege/Ölen auf Wunsch'
+      ],
+      gallery: [
+        { src: 'assets/img/gartenhaus.jpg',  alt: 'Terrasse mit Sitzgruppe' },
+        { src: 'assets/img/dielen.jpg', alt: 'Dielen – Detail' }
+      ]
+    },
+    {
+      id: 'sichtschutz',
+      title: 'Sichtschutz',
+      intro: 'Individuelle Sichtschutzelemente – stabil montiert und genau ausgerichtet.',
+      image: 'assets/img/sichtschutz.jpg',
+      points: [
+        'Pfostenanker/Fundamentlösung',
+        'Maßanpassungen & Ausschnitte',
+        'Stahl-/Alurahmen möglich'
+      ],
+      gallery: [
+        { src: 'assets/img/sichtschutz_1.jpg', alt: 'Sichtschutz mit Metallpfosten' },
+        { src: 'assets/img/sichtschutz_2.jpg', alt: 'Sichtschutz auf Mauer' }
+      ]
+    }
+  ];
 }
--- src/app/component/startpage/startpage.component.ts
+++ src/app/component/startpage/startpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from "@angular/material/icon";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-startpage',
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink],
   templateUrl: './startpage.component.html',
   styles: ``
 })
